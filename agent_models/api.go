6c223d8d3822af810b82b54f104586426d767d34
Shaun Agostinho <shaunagostinho@gmail.com>
2022-09-03 11:31:32 -0700

    update api config response to include verification and such

--- agent_models/api.go
+++ agent_models/api.go
@@ -6,7 +6,9 @@ type ApiResponse struct {
 }
 
 type ApiConfigResponse struct {
-	Response int         `json:"response"`
-	Error    string      `json:"error"`
-	Config   AgentConfig `json:"data"`
+	Response  int         `json:"response"`
+	Error     string      `json:"error"`
+	Config    AgentConfig `json:"data"`
+	NewAgent  bool        `json:"new_agent"`
+	AgentHash string      `json:"agent_hash"`
 }
