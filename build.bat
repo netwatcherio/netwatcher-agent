503b72d9483a15a7a63d96479746917c770aa204
Shaun Agostinho <shaunagostinho@gmail.com>
2022-09-21 20:50:46 -0700

    update windows build stuff

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 echo Building go...
 go build -o ./bin/windows
 echo Building .msi
-go-msi make --msi NetWatcher-Agent.msi --path ./wix/wix.json --src ./wix --out ./bin/windows --version 1.0.1
+go-msi make --msi ./bin/windows/netwatcher-agent_install.msi --path ./wix/wix.json --src ./wix --out ./bin/windows --version 1.0.1
