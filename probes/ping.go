d8e3cc0ded74cfc2fa72578e7d0e0944c33d2049
Shaun Agostinho <shaunagostinho@gmail.com>
2023-11-07 20:10:34 -0800

    fix ping output

--- probes/ping.go
+++ probes/ping.go
@@ -48,11 +48,11 @@ func Ping(ac *Probe, pingChan chan PingResult) {
 	}
 	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
 
-	fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+	/*fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
 	fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
 		stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
 	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
-		stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+		stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)*/
 
 	pingR := PingResult{
 		StartTimestamp:        startTime,
